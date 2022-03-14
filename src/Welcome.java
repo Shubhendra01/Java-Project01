e1761b2714305649cd0079e06b1e1a73787a920d
unknown <shubhhhh17@gmail.com>
2022-03-14 18:12:15 +0530

    Thankyou

---
+++ src/Welcome.java
@@ -0,0 +1,14 @@
+public class Welcome {
+
+    public static void main(String[] args)
+    {
+        String name="hello world";
+        Integer a=10, b=12, c;
+        c=a+b;
+
+        System.out.println(name + " Rahul");
+
+
+        System.out.println(c);
+    }
+}
